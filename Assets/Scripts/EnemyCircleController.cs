bca583d8779374d1a4b4d9d65f1bc4565655e971
Naufal Aulia <naufal7600@gmail.com>
2021-10-11 06:24:50 +0700

    add enemy controller and enemy prefab
    - also update tag for player circle on scenes

---
+++ Assets/Scripts/EnemyCircleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCircleController : MonoBehaviour
+{
+    public CircleMovement circleMovement;
+    public PlayerCircleController playerCircle;
+
+    private Vector2 _targetPos;
+    private float _trackingCooldown = 0.3f;
+    private float _trackingTimer;
+
+    private void Start()
+    {
+        circleMovement = GetComponent<CircleMovement>();
+        playerCircle = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCircleController>();
+        
+        _targetPos = playerCircle.transform.position;
+        _trackingTimer = 0.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        _trackingTimer += Time.deltaTime;
+        if (_trackingTimer >= _trackingCooldown)
+        {
+            _targetPos = playerCircle.transform.position;
+            _trackingTimer = 0.0f;
+        }
+        
+        circleMovement.Move(_targetPos);
+    }
+}
